e125853a3d9740419a7899b439a7708a9bb9a119
Cirdes Henrique <cirdes@gmail.com>
2014-07-06 16:44:10 -0300

    Move label to folder
    Create a folder to labels instead for using as string

--- source/js/services/dymo_service.js
+++ source/js/services/dymo_service.js
@@ -2,54 +2,14 @@
    'use strict';
 
   SelfCheckin.Services.
-    factory('dymoprinter',[function() {
-    var labelXml = '<?xml version="1.0" encoding="utf-8"?>\
-        <DieCutLabel Version="8.0" Units="twips">\
-          <PaperOrientation>Landscape</PaperOrientation>\
-          <Id>LargeAddress</Id>\
-          <PaperName>30321 Large Address</PaperName>\
-          <DrawCommands>\
-            <RoundRectangle X="0" Y="0" Width="2025" Height="5020" Rx="270" Ry="270"/>\
-          </DrawCommands>\
-          <ObjectInfo>\
-            <TextObject>\
-              <Name>Text</Name>\
-              <ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
-              <BackColor Alpha="0" Red="255" Green="255" Blue="255"/>\
-              <LinkedObjectName></LinkedObjectName>\
-              <Rotation>Rotation0</Rotation>\
-              <IsMirrored>False</IsMirrored>\
-              <IsVariable>False</IsVariable>\
-              <HorizontalAlignment>Left</HorizontalAlignment>\
-              <VerticalAlignment>Middle</VerticalAlignment>\
-              <TextFitMode>AlwaysFit</TextFitMode>\
-              <UseFullFontHeight>True</UseFullFontHeight>\
-              <Verticalized>False</Verticalized>\
-              <StyledText>\
-                <Element>\
-                  <String>C</String>\
-                  <Attributes>\
-                    <Font Family="Lucida Grande" Size="13" Bold="False" Italic="False" Underline="False" Strikeout="False"/>\
-                    <ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
-                  </Attributes>\
-                </Element>\
-                <Element>\
-                  <String>irdes henrique</String>\
-                  <Attributes>\
-                    <Font Family="Lucida Grande" Size="13" Bold="False" Italic="False" Underline="False" Strikeout="False"/>\
-                    <ForeColor Alpha="255" Red="0" Green="0" Blue="0"/>\
-                  </Attributes>\
-                </Element>\
-              </StyledText>\
-            </TextObject>\
-            <Bounds X="321.5997" Y="57.59995" Width="4612.8" Height="1881.6"/>\
-          </ObjectInfo>\
-        </DieCutLabel>';
-
-    var printerName = "";
-    var label = dymo.label.framework.openLabelXml(labelXml);
-
-
+    factory('dymoprinter',['$http',function($http) {
+      var labelXml, printerName, label;
+      $http.get('/labels/default.label').success(function(data, status, headers, config) {
+        labelXml = data;
+        label = dymo.label.framework.openLabelXml(labelXml);
+        console.log('Label loaded');
+      });
+      
     // select printer to print on
     // for simplicity sake just use the first LabelWriter printer
     var printers = dymo.label.framework.getPrinters();
@@ -74,7 +34,7 @@
     var dymoprinter = {
       print: function(name){
         // set label text
-        label.setObjectText("Text", name);
+        label.setObjectText("TEXT", name);
         label.print(printerName);
       }
     };
